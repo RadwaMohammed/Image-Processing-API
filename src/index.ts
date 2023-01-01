71fbcf33acacd776304a140f156ea105aa18518f
radwa <mail2radwa@gmail.com>
2023-01-01 13:17:10 +0200

    chore: Set up the server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const greet = 'Hello';
-console.log(greet);
+import express from 'express';
+
+// Create the global application object
+const app = express();
+const port = 3000;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('server working!!');
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
